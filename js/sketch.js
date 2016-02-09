2a342f7c3f7e0d65be53f8ae2e1c3724853c9d99
Radamés Ajna <radamajna@gmail.com>
2016-02-09 22:09:25 +0000

    wireframe and constraints

--- js/sketch.js
+++ js/sketch.js
@@ -7,17 +7,32 @@ function setup() {
   textSize(15)
   noStroke();
   for(var i = 0; i < nParam; i++){
-	  sliders[i] = createSlider(0, 100,100);
-	  sliders[i].position(20, 30+30*i);
+	  sliders[i] = createSlider(0, 100,random(100));
+	  sliders[i].position(20, 30+35*i);
 	  sliders[i].changed(slideChange);
 	  sliders[i].attribute("name", "sld-"+i);
-   }	
+  }
+  for(var i = 0; i < nParam; i++){
+	  sliders[i].elt.dispatchEvent(new Event('change'));
+  }
+	
+	textFont("Helvetica");
+	textSize(15);
+
   }
   
  function draw() {
 	  
 	background(255);
-	drawBody(width/2, height/2, sliders)
+	fill(0);
+	textAlign(LEFT,TOP);
+	text("Intelligence", 160, 30 + 35*0);
+	text("Strength", 160, 30 + 35*1);
+	text("Speed", 160, 30 + 35*2);
+	text("Stamina", 160, 30 + 35*3);
+	text("Empathy", 160, 30 + 35*4);
+
+	drawBody(width/2, height/2, sliders);
 
 }
 
@@ -25,13 +40,12 @@ function setup() {
 function drawBody(px, py, sliders){	
 	push();
 		translate(px,py);
-	
 		drawTorso(0,20, sliders[4].value()/100.0);
-		drawHead(0,-20, sliders[0].value()/100.0);
-		drawLeftArm(10,0,sliders[1].value()/100.0);
-		drawRightArm(-10,0,1);
-		drawRightLeg(-10,50,1);
-		drawLeftLeg(10,50,1);
+		drawHead(0,-20, 1+sliders[0].value()/100.0);
+		drawLeftArm(10,0, sliders[1].value()/100.0);
+		drawRightArm(-10,0, sliders[1].value()/100.0);
+		drawRightLeg(-10,50,sliders[2].value()/100.0);
+		drawLeftLeg(10,50,sliders[2].value()/100.0);
 
 	pop();
 }
@@ -40,8 +54,9 @@ function drawHead(px, py, s){
 	push();
 	
 		translate(px,py);
-		scale(s)
-		fill(255,0,0)
+		scale(s);
+		noFill();
+		stroke(0);
 		ellipse(0, 0, 30, 30);
 	
 	pop();
@@ -53,7 +68,8 @@ function drawLeftArm(px, py, s){
 		ellipseMode(CORNER);	
 		translate(px,py);
 		scale(s)
-		fill(200,0,0)
+		noFill();
+		stroke(0);
 		ellipse(0, -10, 50, 20);
 		push();
 			ellipseMode(CORNER);
@@ -72,8 +88,9 @@ function drawRightArm(px, py, s){
 		translate(px,py);
 		rotate(PI);
 		scale(s)
-		fill(200,0,0)
-		ellipse(0, -10, 50, 20);
+		noFill();
+		stroke(0);
+	    ellipse(0, -10, 50, 20);
 		push();
 			ellipseMode(CORNER);
 			translate(45,0);
@@ -89,7 +106,10 @@ function drawRightLeg(px, py, s){
 		translate(px,py);
 		rotate(2*PI/3);
 		scale(s)
-		fill(200,0,0)
+		
+		noFill();
+		stroke(0);
+	
 		ellipse(0, -10, 50, 20);
 		push();
 			ellipseMode(CORNER);
@@ -106,7 +126,9 @@ function drawLeftLeg(px, py, s){
 		translate(px,py);
 		rotate(PI/4);
 		scale(s)
-		fill(200,0,0)
+		noFill();
+		stroke(0);
+	
 		ellipse(0, -10, 50, 20);
 		push();
 			ellipseMode(CORNER);
@@ -123,7 +145,8 @@ function drawTorso(px, py, s){
 		translate(px,py);
 		rotate(PI);
 		scale(s);
-		fill(100,0,0);
+		noFill();
+		stroke(0);
 		rect(0,0,40,60);
 	pop();
 }
@@ -133,9 +156,18 @@ function drawTorso(px, py, s){
 function slideChange(e) {
 	switch(e.target.name){
 		case "sld-0":
-			sliders[0].elt.value = 100-map(sliders[0].value(), 0, 100, 0, sliders[1].value());
+			sliders[1].elt.value = 100 - sliders[0].value();
 			break;
 		case "sld-1":
+			sliders[0].elt.value = 100 - sliders[1].value();
+			break;
+		case "sld-2":
+			sliders[3].elt.value = 100 - sliders[2].value();
+			break;
+		case "sld-3": 
+			sliders[2].elt.value = 100 - sliders[3].value();
+			break;
+		case "sld-4":
 			break;
 	}
 
